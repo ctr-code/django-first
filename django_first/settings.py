4d72bb63514ae9452bd40cd3c453fba8a1829f59
ctr-code <codeinstitute@charlesreilly.com>
2026-08-24 16:34:18 +0100

    Prepare for heroku deployment

--- django_first/settings.py
+++ django_first/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$+-1y0szhz6p1wcb#iv&p!f&byqr-#zm_f_)denh)46p+mp^o+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# To debug, define environment variable DEBUG_DJANGO=True
+DEBUG = os.getenv('DEBUG_DJANGO', False) == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    '.herokuapp.com',
+]
 
 
 # Application definition
